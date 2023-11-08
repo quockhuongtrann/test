1300f2318095e09a3bb3f4ad590bb61ee578acd2
Quoc-Khuong Tran <quockhuongtran17@gmail.com>
2023-11-08 18:37:19 +0700

    init commit

---
+++ starter/assignment.js
@@ -0,0 +1,10 @@
+const country = 'Vietnam';
+const continent = 'Asia';
+let population = 100;
+const isIsland = false;
+let language = 'Vietnamese';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+console.log(isIsland);
